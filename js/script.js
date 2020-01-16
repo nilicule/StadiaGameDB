e0a4284f815f13d8ba15bdb44e2624ac1939e8eb
Remco Brink <remco@net2grid.com>
2020-01-16 11:07:36 +0100

    StadiaGameDB-10 Add rounded corners for thumbnails

---
+++ js/script.js
@@ -0,0 +1,29 @@
+        $(document).ready(function() {
+            var query = getQueryParams(document.location.search);
+
+	        var stadiaTable = $('#example').DataTable( {
+		        "ajax": 'data/gamedb.json',
+		        "iDisplayLength": 100,
+		        "columnDefs": [
+                    { "searchable": false, "targets": 0 },
+                    { "orderable": false, "targets": 0 }
+                ]
+	        } );
+
+            if (query.filter != null) {
+                stadiaTable.search(query.filter).draw();
+            }
+        } );
+
+        function getQueryParams(qs) {
+	        qs = qs.split('+').join(' ');
+	        var params = 	{},
+    			tokens,
+    			re = /[?&]?([^=]+)=([^&]*)/g;
+
+        	while (tokens = re.exec(qs)) {
+        		params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+	        }
+
+        	return params;
+        }
